c103da90652b32b50cb579109b65975b3fe15c82
joiellantero <jtllantero@up.edu.ph>
2022-02-15 21:13:21 +0800

    add tooltip

--- src/Components/Buttons/ButtonPrimary.js
+++ src/Components/Buttons/ButtonPrimary.js
@@ -1,11 +1,19 @@
 const ButtonPrimary = (props) => {
   return (
-    <button
-      className="py-2 px-4 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white focus:outline-none rounded"
+    <div className="relative my-5 group">
+      <button
+      className="py-2 px-4 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white focus:outline-none rounded disabled:opacity-75 disabled:cursor-not-allowed"
       onClick={props.onClick}
-    >
-      {props.value}
-    </button>
+      disabled={props.disabled}
+      >
+        {props.value}
+      </button>
+      {props.disabled && (
+        <span className='tooltip group-hover:scale-100'>
+          {props.tooltip}
+        </span>
+      )}
+    </div>
   );
 }
  
