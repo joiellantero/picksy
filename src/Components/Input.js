cd201fe13d4b2ee384f406125507e50f2ddd970f
joiellantero <jtllantero@up.edu.ph>
2022-02-16 21:16:59 +0800

    set max characters

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -12,6 +12,7 @@ const Input = (props) => {
         rows="5"
         className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 dark:text-gray-300 bg-clip-padding dark:bg-slate-800 bg-slate-100 rounded transition ease-in-out m-0 focus:outline-none"
         onChange={(e) => props.onChange(e.target.value)}
+        maxLength={60}
       />
     </div>
   );
