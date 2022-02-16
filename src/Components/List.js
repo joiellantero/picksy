c2fd3e720d0b9a3d1bdde4c7828901509a6282f2
joiellantero <jtllantero@up.edu.ph>
2022-02-16 21:22:20 +0800

    increase rows

--- src/Components/List.js
+++ src/Components/List.js
@@ -15,7 +15,7 @@ const List = (props) => {
         <textarea
           className="form-control block px-3 py-1.5 text-base font-normal text-gray-700 dark:text-gray-300 bg-clip-padding dark:bg-slate-800 bg-slate-100 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-slate-200 focus:outline-none"
           id="listInput"
-          rows="5"
+          rows="10"
           onChange={(e) => props.onChange(e.target.value)}
         />
       </div>
