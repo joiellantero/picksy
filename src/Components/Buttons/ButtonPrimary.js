cda15f9e027169cb22c2d8db1b0b8cea9370c122
joiellantero <jtllantero@up.edu.ph>
2022-02-16 20:30:13 +0800

    put tooltip on top of button

--- src/Components/Buttons/ButtonPrimary.js
+++ src/Components/Buttons/ButtonPrimary.js
@@ -1,6 +1,11 @@
 const ButtonPrimary = (props) => {
   return (
     <div className="relative my-5 group">
+      {props.disabled && (
+          <span className='tooltip group-hover:scale-100'>
+        {props.tooltip}
+      </span>
+      )}
       <button
       className="py-2 px-4 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white focus:outline-none rounded disabled:opacity-75 disabled:cursor-not-allowed"
       onClick={props.onClick}
@@ -8,11 +13,6 @@ const ButtonPrimary = (props) => {
       >
         {props.value}
       </button>
-      {props.disabled && (
-        <span className='tooltip group-hover:scale-100'>
-          {props.tooltip}
-        </span>
-      )}
     </div>
   );
 }
