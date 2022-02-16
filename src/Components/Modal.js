f1d86b8c14d2ffc8ce1abe3f67d892f786278325
joiellantero <jtllantero@up.edu.ph>
2022-02-16 21:01:22 +0800

    update animation of closing the modal

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -2,6 +2,8 @@ import { Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react';
 import { useDarkMode } from '../shared/globalState';
 
+import CloseIcon from '../assets/icons/CloseIcon';
+
 const Modal = (props) => {
   const [isDarkModeEnabled] = useDarkMode();
 
@@ -41,27 +43,26 @@ const Modal = (props) => {
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden align-middle transition-all transform dark:bg-slate-700 bg-white shadow-md rounded text-center">
+            <div
+              className="inline-block w-full max-w-md p-10 my-8 overflow-hidden align-middle duration-100 transition-all ease-in-out transform dark:bg-slate-700 bg-white shadow-md rounded text-center hover:border-4 dark:border-slate-600 border-slate-200 hover:cursor-pointer group"
+              onClick={() => props.onClose(false)}
+            >
               <Dialog.Title
                 as="h3"
                 className="text-base font-medium leading-6 text-slate-400 dark:text-slate-400"
               >
                 {props.title}
               </Dialog.Title>
-              <div className="mt-2">
+              <div className="mt-5">
                 <p className="text-5xl text-blue-700 dark:text-slate-100">
                   {props.body}
                 </p>
               </div>
 
-              <div className="mt-4 flex gap-2 justify-center">
-                <button
-                  type="button"
-                  className="inline-flex justify-center px-4 py-2 text-sm font-medium text-slate-500 dark:text-slate-400 bg-slate-100 dark:bg-slate-600 border border-transparent rounded hover:bg-slate-200 dark:hover:bg-slate-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-slate-500"
-                  onClick={() => props.onClose(false)}
-                >
-                  Close
-                </button>
+              <div
+                className="absolute top-4 right-4 justify-center text-sm font-medium text-slate-500 dark:text-slate-400 border border-transparent rounded focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-slate-500 scale-0 group-hover:scale-100 transition-all transform origin-top-right"
+              >
+                <CloseIcon color={`${isDarkModeEnabled ? '#64748b' : '#cbd5e1'}`}/>
               </div>
             </div>
           </Transition.Child>
