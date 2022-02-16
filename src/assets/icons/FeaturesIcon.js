0bb25633f5b76bf5f93c6938c53029d325b8c93f
joiellantero <jtllantero@up.edu.ph>
2022-02-16 21:43:49 +0800

    isolate icons from nav

---
+++ src/assets/icons/FeaturesIcon.js
@@ -0,0 +1,34 @@
+const FeaturesIcon = () => {
+  return(
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="48" height="48" rx="4" fill="#BFDBFE" />
+      <path
+        d="M24 11L35.2583 17.5V30.5L24 37L12.7417 30.5V17.5L24 11Z"
+        stroke="#346FED"
+        strokeWidth="2"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M16.7417 19.8094V28.1906L24 32.3812L31.2584 28.1906V19.8094L24 15.6188L16.7417 19.8094Z"
+        stroke="#0284c7"
+        strokeWidth="2"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M20.7417 22.1196V25.882L24 27.7632L27.2584 25.882V22.1196L24 20.2384L20.7417 22.1196Z"
+        stroke="#0284c7"
+        strokeWidth="2"
+      />
+    </svg>
+  )
+}
+
+export default FeaturesIcon
---
+++ src/assets/icons/FeedbackIcon.js
@@ -0,0 +1,21 @@
+const FeedbackIcon = () => {
+  return(
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="48" height="48" rx="4" fill="#BFDBFE" />
+      <rect x="13" y="32" width="2" height="4" fill="#0284c7" />
+      <rect x="17" y="28" width="2" height="8" fill="#0284c7" />
+      <rect x="21" y="24" width="2" height="12" fill="#0284c7" />
+      <rect x="25" y="20" width="2" height="16" fill="#0284c7" />
+      <rect x="29" y="16" width="2" height="20" fill="#346FED" />
+      <rect x="33" y="12" width="2" height="24" fill="#346FED" />
+    </svg>
+  )
+}
+
+export default FeedbackIcon;
