ae4f92497e8f7476ef56893d5e8e1cf0694b4129
joiellantero <jtllantero@up.edu.ph>
2022-02-17 17:54:21 +0800

    fix darkmode colors

--- src/Components/Nav/MobileNav.js
+++ src/Components/Nav/MobileNav.js
@@ -10,12 +10,12 @@ const MenuWindow = () => {
   const toggleDarkMode = () => setIsDarkModeEnabled(!isDarkModeEnabled);
 
   return(
-    <div className="w-56 text-right fixed bottom-[23.5rem] right-8 z-10">
+    <div className="w-56 text-right fixed bottom-[22rem] right-8 z-10">
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className="visible md:invisible md:hidden hover:cursor-pointer bg-blue-600 p-4 rounded-full fixed bottom-12 right-8 text-white z-10 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+          <Menu.Button className="visible md:invisible md:hidden hover:cursor-pointer bg-blue-600 p-4 rounded-full fixed bottom-10 right-8 text-white z-10 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
             <MenuIcon
-              height={25}
+              height={30}
             />
           </Menu.Button>
         </div>
@@ -28,14 +28,14 @@ const MenuWindow = () => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 w-56 mt-2 origin-bottom-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="absolute right-0 w-56 mt-2 origin-bottom-right bg-slate-100 dark:bg-slate-700 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="px-1 py-1 ">
               <Menu.Item>
                 {({ active }) => (
                   <Link to="/">
                     <button
                       className={`${
-                        active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                        active ? 'bg-blue-500 text-white' : 'text-gray-900 dark:text-slate-200'
                       } group flex rounded-md items-center w-full px-2 py-2 text-sm leading-10`}
                     >
                       {active ? (
@@ -44,10 +44,17 @@ const MenuWindow = () => {
                           aria-hidden="true"
                         />
                       ) : (
-                        <HomeInactiveIcon
-                          className="w-5 h-5 mr-2"
-                          aria-hidden="true"
-                        />
+                        isDarkModeEnabled ? (
+                          <DarkHomeInactiveIcon
+                            className="w-5 h-5 mr-2"
+                            aria-hidden="true"
+                          />
+                        ) : (
+                          <HomeInactiveIcon
+                            className="w-5 h-5 mr-2"
+                            aria-hidden="true"
+                          />
+                        )
                       )}
                       Home
                     </button>
@@ -58,7 +65,7 @@ const MenuWindow = () => {
                 {({ active }) => (
                   <button
                     className={`${
-                      active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                      active ? 'bg-blue-500 text-white' : 'text-gray-900 dark:text-slate-200'
                     } group flex rounded-md items-center w-full px-2 py-2 text-sm leading-10`}
                   >
                     {active ? (
@@ -67,22 +74,27 @@ const MenuWindow = () => {
                         aria-hidden="true"
                       />
                     ) : (
-                      <HelpInactiveIcon
-                        className="w-5 h-5 mr-2"
-                        aria-hidden="true"
-                      />
+                      isDarkModeEnabled ? (
+                        <DarkHelpInactiveIcon
+                          className="w-5 h-5 mr-2"
+                          aria-hidden="true"
+                        />
+                      ): (
+                        <HelpInactiveIcon
+                          className="w-5 h-5 mr-2"
+                          aria-hidden="true"
+                        />
+                      )
                     )}
                     Help
                   </button>
                 )}
               </Menu.Item>
-            </div>
-            <div className="px-1 py-1">
               <Menu.Item>
                 {({ active }) => (
                   <button
                     className={`${
-                      active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                      active ? 'bg-blue-500 text-white' : 'text-gray-900 dark:text-slate-200'
                     } group flex rounded-md items-center w-full px-2 py-2 text-sm leading-10`}
                     onClick={toggleDarkMode}
                   >
@@ -93,10 +105,17 @@ const MenuWindow = () => {
                           aria-hidden="true"
                         />
                       ) : (
-                        <MoonInactiveIcon
-                          className="w-5 h-5 mr-2"
-                          aria-hidden="true"
-                        />
+                        isDarkModeEnabled ? (
+                          <DarkMoonInactiveIcon
+                            className="w-5 h-5 mr-2"
+                            aria-hidden="true"
+                          />
+                        ) : (
+                          <MoonInactiveIcon
+                            className="w-5 h-5 mr-2"
+                            aria-hidden="true"
+                          />
+                        )
                       )
                     ) : (
                       active ? (
@@ -119,7 +138,7 @@ const MenuWindow = () => {
                 {({ active }) => (
                   <button
                     className={`${
-                      active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                      active ? 'bg-blue-500 text-white' : 'text-gray-900 dark:text-slate-200'
                     } group flex rounded-md items-center w-full px-2 py-2 text-sm leading-10`}
                   >
                     {active ? (
@@ -128,10 +147,17 @@ const MenuWindow = () => {
                         aria-hidden="true"
                       />
                     ) : (
-                      <SettingsInactiveIcon
-                        className="w-5 h-5 mr-2"
-                        aria-hidden="true"
-                      />
+                      isDarkModeEnabled ? (
+                        <DarkSettingsInactiveIcon
+                          className="w-5 h-5 mr-2"
+                          aria-hidden="true"
+                        />
+                      ) : (
+                        <SettingsInactiveIcon
+                          className="w-5 h-5 mr-2"
+                          aria-hidden="true"
+                        />
+                      )
                     )}
                     Settings
                   </button>
@@ -155,6 +181,24 @@ function HomeInactiveIcon(props) {
     >
       <path
         fill="#dbeafe"
+        stroke="#60a5fa"
+        strokeWidth="2"
+        d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+      />
+    </svg>
+  )
+}
+
+function DarkHomeInactiveIcon(props) {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill="#334155"
         stroke="#60a5fa"
         strokeWidth="2"
         d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
@@ -192,6 +236,26 @@ function HelpInactiveIcon(props) {
       <path
         d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
         fill="#dbeafe"
+        stroke="#60a5fa"
+        strokeWidth="2"
+        strokeLinecap={'round'}
+        strokeLinejoin={'round'}
+      />
+    </svg>
+  )
+}
+
+function DarkHelpInactiveIcon(props) {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        fill="#334155"
         stroke="#60a5fa"
         strokeWidth="2"
         strokeLinecap={'round'}
@@ -281,6 +345,26 @@ function MoonInactiveIcon(props) {
   )
 }
 
+function DarkMoonInactiveIcon(props) {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        fill="#334155"
+        stroke="#60a5fa"
+        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+      />
+    </svg>
+  )
+}
+
 function MoonActiveIcon(props) {
   return (
     <svg
@@ -313,7 +397,7 @@ function SettingsInactiveIcon(props) {
         strokeLinecap={"round"}
         strokeLinejoin={"round"}
         strokeWidth="2"
-        fill="#dbeafe"
+        fill={'#dbeafe'}
         stroke="#60a5fa"
         d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
       />
@@ -322,6 +406,34 @@ function SettingsInactiveIcon(props) {
         strokeLinejoin={"round"}
         strokeWidth="2"
         fill="#dbeafe"
+        stroke="#60a5fa"
+        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+      />
+    </svg>
+  )
+}
+
+function DarkSettingsInactiveIcon(props) {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap={"round"}
+        strokeLinejoin={"round"}
+        strokeWidth="2"
+        fill="#334155"
+        stroke="#60a5fa"
+        d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+      />
+      <path
+        strokeLinecap={"round"}
+        strokeLinejoin={"round"}
+        strokeWidth="2"
+        fill="#334155"
         stroke="#60a5fa"
         d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
       />
