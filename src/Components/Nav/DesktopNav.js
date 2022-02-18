44a493214bd4bea1f1b548e5d8bc56f951332f7c
joiellantero <jtllantero@up.edu.ph>
2022-02-18 08:10:32 +0800

    update logo size

--- src/Components/Nav/DesktopNav.js
+++ src/Components/Nav/DesktopNav.js
@@ -50,7 +50,7 @@ const DesktopNav = () => {
 
   return(
     <div className='text-center md:text-left md:flex md:justify-between px-12 md:px-14 lg:px-24 py-10'>
-      <div className='py-2 flex justify-center items-center font-serif font-bold text-2xl md:text-lg focus:outline-none  focus-visible:ring-2 focus-visible:ring-blue-600 dark:focus-visible:ring-white focus-visible:ring-opacity-75'>
+      <div className='py-2 flex justify-center items-center font-serif font-bold text-lg focus:outline-none  focus-visible:ring-2 focus-visible:ring-blue-600 dark:focus-visible:ring-white focus-visible:ring-opacity-75'>
         <Link to="/">
           <Logo />
         </Link>
