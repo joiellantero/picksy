008361490f2608e308c9e72c458d7ad2b635b062
joiellantero <jtllantero@up.edu.ph>
2022-02-20 00:16:36 +0800

    fix textarea width

--- src/Components/TextArea.js
+++ src/Components/TextArea.js
@@ -2,7 +2,7 @@ const TextArea = (props) => {
   return(
     props.readOnly ? (
         <textarea
-          className="form-control block w-80 md:w-60 px-3 py-1.5 text-base font-normal text-gray-700 dark:text-gray-300 bg-clip-padding dark:bg-slate-800 bg-slate-200 rounded transition ease-in-out m-0 focus:outline-none"
+          className="form-control block w-80 md:w-full max-w-xs px-3 py-1.5 text-base font-normal text-gray-700 dark:text-gray-300 bg-clip-padding dark:bg-slate-800 bg-slate-200 rounded transition ease-in-out m-0 focus:outline-none"
           value={props.value}
           rows={props.rows}
           id={props.id}
@@ -11,7 +11,7 @@ const TextArea = (props) => {
         />
       ) : (
         <textarea
-          className="form-control block px-3 py-1.5 w-80 md:w-60 text-base font-normal text-slate-700 dark:text-slate-300 bg-clip-padding dark:bg-slate-800 bg-slate-200 rounded transition ease-in-out m-0 focus:text-slate-700 focus:bg-slate-300 dark:focus:bg-[#162338] focus:outline-none"
+          className="form-control block px-3 py-1.5 w-80 md:w-full max-w-xs text-base font-normal text-slate-700 dark:text-slate-300 bg-clip-padding dark:bg-slate-800 bg-slate-200 rounded transition ease-in-out m-0 focus:text-slate-700 focus:bg-slate-300 dark:focus:bg-[#162338] focus:outline-none"
           value={props.value}
           rows={props.rows}
           id={props.id}
