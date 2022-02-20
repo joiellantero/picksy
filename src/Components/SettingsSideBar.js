e6b5b45388b6376cf83bc83be33c5640ee3da942
joiellantero <jtllantero@up.edu.ph>
2022-02-20 15:06:43 +0800

    update ui

--- src/Components/SettingsSideBar.js
+++ src/Components/SettingsSideBar.js
@@ -1,19 +1,17 @@
 // TODO create winner message component and clear message button
 
 import { useRecoilState, useResetRecoilState } from "recoil";
-import {useRemoveState, useSettingsModalState, namesListState, winnerMessageState} from "../shared/globalState";
+import {useRemoveState, useSettingsModalState, namesListState} from "../shared/globalState";
 
 import List from "./List";
-import Input from "./Input";
 import Toggle from "./Toggle";
+import WinnerMessage from "./WinnerMessage";
 
 const SettingsSideBar = () => {
   const [shouldRemoveName, setShouldRemoveName] = useRemoveState();
   const [settingsSideBarState] = useSettingsModalState();
   const [names, setName] = useRecoilState(namesListState);
   const resetNamesList = useResetRecoilState(namesListState);
-  const [winnerMessage, setWinnerMessage] = useRecoilState(winnerMessageState);
-  const resetWinnerMessage = useResetRecoilState(winnerMessageState);
 
   return(
     <div className={`min-h-screen bg-slate-300/70 dark:bg-slate-700/70 p-10 absolute right-0 transform transition ${settingsSideBarState ? 'translate-x-0 ' : 'translate-x-full lg:translate-x-0' } duration-200 ease-in-out flex flex-col items-center`}
@@ -27,13 +25,9 @@ const SettingsSideBar = () => {
           onChange={(e) => setName(e)}
           onClear={() => resetNamesList()}
         />
-        <Input
-          id={'winnerMessage'}
-          label={'Winner Prompt Message'}
-          value={winnerMessage}
-          onChange={(e) => setWinnerMessage(e)}
-          onClear={() => resetWinnerMessage()}
-        />
+        <hr/>
+        <WinnerMessage/>
+        <hr/>
         <Toggle
           isOn={shouldRemoveName}
           handleToggle={() => setShouldRemoveName(!shouldRemoveName)}
