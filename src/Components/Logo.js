8b3316b3232ff386e11245f8ee43fc8682ce71e1
joiellantero <jtllantero@up.edu.ph>
2022-02-21 11:10:23 +0800

    update logo

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,11 +1,14 @@
 const Logo = () => {
   return(
-    <>
-      Name Roulette
-      <span className={'bg-blue-600 ml-1.5 px-1 pt-1 rounded text-white'}>
+    <div className='py-2 flex justify-center items-center font-serif font-bold focus:outline-none  focus-visible:ring-2 focus-visible:ring-blue-600 dark:focus-visible:ring-white focus-visible:ring-opacity-75'
+    >
+      <div className={'pt-0.5'}>
+        Name Roulette
+      </div>
+      <small className={'bg-blue-600 ml-1.5 px-2.5 py-0 pt-1 rounded-full text-white'}>
         Web
-      </span>
-    </>
+      </small>
+    </div>
   )
 }
 
