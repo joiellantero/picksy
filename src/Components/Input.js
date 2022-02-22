eb548bcdd8bc28220c0d465392e832f3455a4e1f
joiellantero <jtllantero@up.edu.ph>
2022-02-22 23:58:20 +0800

    fix padding right

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -11,14 +11,14 @@ const Input = (props) => {
         <input
           id={props.id}
           rows="5"
-          className="form-control block max-w-xs px-3 py-1.5 text-base font-normal text-gray-700 dark:text-gray-300 bg-clip-padding dark:bg-slate-800 bg-slate-200 focus:bg-slate-300 dark:focus:bg-[#162338] rounded transition ease-in-out m-0 focus:outline-none h-14 md:h-fit w-full"
+          className="form-control block max-w-xs pl-3 pr-14 py-1.5 text-base font-normal text-gray-700 dark:text-gray-300 bg-clip-padding dark:bg-slate-800 bg-slate-200 focus:bg-slate-300 dark:focus:bg-[#162338] rounded transition ease-in-out m-0 focus:outline-none h-14 md:h-fit w-full"
           onChange={(e) => props.onChange(e.target.value)}
           value={props.value}
           maxLength={60}
         />
         {props.value.length !== 0 &&(
           <button
-            className={'absolute top-0 right-0 rounded-full z-10 text-white top-1/2 transform -translate-y-1/2 mr-3 cursor-pointer text-xs bg-slate-400/50 hover:bg-slate-400 dark:bg-slate-700/50 dark:hover:bg-slate-700 py-1 px-1.5 transition transition-all duration-200 ease-out'}
+            className={'absolute -right-1 rounded-full z-10 text-white top-1/2 transform -translate-y-1/2 mr-3 cursor-pointer text-xs bg-slate-400/50 hover:bg-slate-400 dark:bg-slate-700/50 dark:hover:bg-slate-700 py-1 px-1.5 transition duration-200 ease-out'}
             onClick={props.onClear}
           >
             Clear
