2869cfba01244e098a7f837d05b457d9106d34be
joiellantero <jtllantero@up.edu.ph>
2022-02-23 13:17:18 +0800

    update file upload helper text

--- src/Components/Buttons/ButtonUpload.js
+++ src/Components/Buttons/ButtonUpload.js
@@ -16,21 +16,16 @@ const ButtonUpload = (props) => {
 
   return (
     <form className={`relative group`}>
-      <label class="block">
+      <label className="block">
         <input
-          className="block file:bg-slate-500 file:hover:bg-slate-600 file:focus:outline-none file:active:bg-slate-700 w-full text-sm text-slate-500
-          file:mr-4 file:py-2 file:px-4
-          file:rounded file:border-0
-          file:text-sm file:font-semibold
-          file:text-white
-          file:cursor-pointer"
+          className="block file:bg-slate-500 file:hover:bg-slate-600 file:focus:outline-none file:active:bg-slate-700 w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:text-white file:cursor-pointer"
           type="file"
           id={props.id}
           name={props.name}
           onChange={(e) => showFile(e)}
         />
         <small className="mt-2 text-slate-500">
-          File (.txt/.csv) uploaded are not saved.
+          Files (.txt/.csv) uploaded are not stored online.
         </small>
       </label>
     </form>
